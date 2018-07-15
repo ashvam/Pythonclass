292e09f54f3091b659c5680efb48d30abc3d5181
ashvam <ashva_m@yahoo.com>
2018-07-15 15:30:33 -0700

    changes

--- scrape s&P.py
+++ scrape s&P.py
@@ -1,6 +1,23 @@
-import beautifulsoup
-
 import urllib.request
-page = urllib.request.urlopen('https://www.bloomberg.com/quote/SPX:IND')
+import csv
+from bs4 import BeautifulSoup
+
+symbol = input("name your stock:")
+ticker =  'https://www.nasdaq.com/symbol/'+ symbol
+print(ticker)
+with open('egg.csv', 'wb') as f
+page = urllib.request.urlopen(ticker)
 s = page.read()
-print(s)
+
+soup = BeautifulSoup(s, 'html.parser')
+#print (soup.prettify())
+# print(soup)
+# print(list(soup.title))
+#print(list(soup.p))
+#print(list(soup.a))
+#print(soup.h3)
+#Lead-2-QuoteHeader-Proxy
+#print(id('Lead-2-QuoteHeader-Proxy'))
+#print(soup.find_all('h3'))
+p = soup.find("div", {"id": "qwidget_lastsale"})
+print(p.get_text())
